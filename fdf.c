e30b2d638782720c0bb87499a7946f6f772bc373
Assem Zhilkibayeva <aszhilki@e1z2r5p10.42.us.org>
2019-11-27 21:45:53 -0800

    Updated: check function works

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: aszhilki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 14:30:21 by aszhilki          #+#    #+#             */
-/*   Updated: 2019/11/22 20:30:54 by aszhilki         ###   ########.fr       */
+/*   Updated: 2019/11/27 19:53:31 by aszhilki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static int	ft_check(const int fd, char ***map)
+static int	ft_check(const int fd, char **map)
 {
-	char	**tmp;
-	char	**str;
-	char	**tmp2;
+	char	**check;
+	char	*tmp[2];
+	char	*blank;
 	char	*line;
 	int		rows;
 	int		i;
@@ -27,20 +27,23 @@ static int	ft_check(const int fd, char ***map)
 	rows = 0;
 	i = 0;
 	*map = NULL;
+	blank = " ";
 	while (get_next_line(fd, &line) != 0)
 	{	
-		tmp = ft_strsplit(line, ' ');
-		tmp2 = *map;
-		*map = ft_arrayjoin(*map, tmp);
+		tmp[0] = *map;
+		tmp[1] = ft_strjoin(*map, line);
+		*map = ft_strjoin(tmp[1], blank);
+		ft_strdel(&tmp[0]);
+		ft_strdel(&tmp[1]);
+		check = ft_strsplit(line, ' ');
 		ft_strdel(&line);
-		ft_strdel(tmp2);
-		while (tmp[i])
+		while (check[i])
 			i++;
 		if (rows == 0)
 			rows = i;
 		else if (rows != i)
 			return (0);
-		ft_strdel(tmp);
+		ft_strdel(check);
 		i = 0;
 	}
 	if (rows == 0)
@@ -49,30 +52,42 @@ static int	ft_check(const int fd, char ***map)
 		return (rows);
 }
 
+static	void ft_draw(char **values, int rows)
+{
+		void	*mlx_ptr;
+		void	*win_ptr;
+		int n;
+		n = 0;
+
+		mlx_ptr = mlx_init();
+		win_ptr = mlx_new_window(mlx_ptr, 500, 500, "fdf");
+		while (n < 20)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, 50 + n, 50, 250);
+			n++;
+		}
+		mlx_loop(mlx_ptr);
+}
+
 int		main(int argc, char **argv)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	char	**map;
+	char	*map;
+	char	**values;
 	int		rows;
 	int		fd;
 
+		
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (!(rows = ft_check(fd, &map)))
+		{
 			ft_putstr("Invalid file\n");
-		else
-			printf("%s\n", *map);
+			return (0);
+		}
+		values = ft_strsplit(map, ' ');
+		ft_draw(values, rows);
 
-//		mlx_ptr = mlx_init();
-//		win_ptr = mlx_new_window(mlx_ptr, 500, 500, "fdf");
-//		while (n < 20)
-//		{
-//			mlx_pixel_put(mlx_ptr, win_ptr, 50 + n, 50, 250);
-//			n++;
-//		}
-//		mlx_loop(mlx_ptr);
 	}
 	else
 		ft_putstr("Usage : ./fdf <filename> [ case_size z_size ]\n");
